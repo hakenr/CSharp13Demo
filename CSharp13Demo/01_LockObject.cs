b2ece1b6d29ace8def7a47eda9804ce356cf07c6
Robert Haken <haken@havit.cz>
2024-05-21 08:57:09 +0200

    Lock object update

--- CSharp13Demo/01_LockObject.cs
+++ CSharp13Demo/01_LockObject.cs
@@ -17,7 +17,16 @@ public static class Demo
 		{
 			Console.WriteLine("Locked");
 		}
-		
+
+
+		// new lock pattern
+		using (_lockNew.EnterScope())
+		{
+			Console.WriteLine("Locked");
+		}
+
+
+		// C# 13 support for lock keyword with Lock object
 		lock (_lockNew)  // = using (_lock.EnterScope()) { ... }
 		{
 			Console.WriteLine("Locked");
@@ -30,3 +39,4 @@ public static class Demo
 // https://github.com/dotnet/runtime/issues/34812 - Add first class System.Threading.Lock type
 // "Locking on any class has overhead from the dual role of the syncblock as both lock field and hashcode et al. "
 // "Adding a first class lock type that didn't allow alternative uses and only acted as a lock would allow for a simpler and faster lock as well as be less ambiguous on type and purpose in source code."
+// https://github.com/dotnet/runtime/blob/main/src/libraries/System.Private.CoreLib/src/System/Threading/Lock.cs
