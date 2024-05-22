d6e20850331b7d27c99239a0870800029ba7105a
Robert Haken <haken@havit.cz>
2024-05-22 23:03:16 +0200

    minor updates

--- CSharp13Demo/04_ParamsCollections.cs
+++ CSharp13Demo/04_ParamsCollections.cs
@@ -1,26 +1,19 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ParamsCollections;
+﻿namespace ParamsCollections;
 
 public class Demo
 {
 	// C# 12 - array type only
-	public static void DoSomething(params int[] values)	{ }
+	public static void DoSomething(params int[] values) { }
 
 	// C# 13 - "any" collection type/interface + IEnumerable with Add method
 	public static void DoSomething2(params List<int> values) { }
 	public static void DoSomething3(params Span<int> values) { }
 	public static void DoSomething4(params IEnumerable<int> values) { }
 
-	// calling side - same as collection expressions
+	// calling side - same compiler logic as collection expressions
 	public static void CallDoSomething()
 	{
-		DoSomething4(1, 2, 3 );
+		DoSomething4(1, 2, 3);
 	}
 }
 
