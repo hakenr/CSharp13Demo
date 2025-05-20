6cbf9aa724a97b72ea4acf86955054517a531955
Robert Haken <haken@havit.cz>
2025-05-20 09:50:39 +0200

    Partial properties

---
+++ CSharp13Demo/05_PartialProperties.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PartialProperties;
+
+public partial class Demo
+{
+	// one declaring declaration of the property
+	public partial string PropertyValue { get; set; }
+}
+
+public partial class Demo
+{
+	// one implementing declaration of the property
+	public partial string PropertyValue
+	{
+		get => _propertyValue;
+		set => _propertyValue = value;
+	}
+	private string _propertyValue;
+}
+
+public partial class MyRegExs
+{
+	[GeneratedRegex(".*")]
+	public partial Regex AnyString { get; }
+
+	[GeneratedRegex("^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$")]
+	public partial Regex Email { get; }
+}
